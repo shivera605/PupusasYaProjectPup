9acaaeb1509777ddb862369d76a2c6a49572bbc1
DaviAsaf <david.amaya@catolica.edu.sv>
2019-11-13 07:23:20 -0600

    Spinner OK :o :)

--- app/src/main/java/pupusas/app/pupusasyaprojectpup/MenuProductosActivity.java
+++ app/src/main/java/pupusas/app/pupusasyaprojectpup/MenuProductosActivity.java
@@ -6,10 +6,13 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,10 +29,11 @@ import java.util.ArrayList;
 import cz.msebera.android.httpclient.Header;
 
 public class MenuProductosActivity extends AppCompatActivity {
-private ListView lista;
-private ArrayList nombre, precio;
-private TextView m;
-private String id, resultado;
+    private ListView lista;
+    private ArrayList nombre, precio;
+    private TextView m;
+    private EditText txtSearch;
+    private String id, resultado;
     private boolean status = false;
 
     @Override
@@ -42,16 +46,35 @@ private String id, resultado;
         Intent i = this.getIntent();
         String name = i.getStringExtra("name");
         id = i.getStringExtra("id");
-        m.setText("Menu Pupusería " + name );
+        m.setText("Menu Pupusería " + name);
 
         lista = (ListView) findViewById(R.id.listamenu);
+        txtSearch = findViewById(R.id.etSearchP);
         nombre = new ArrayList();
         precio = new ArrayList();
 
         descargardatos();
+
+        txtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String name = txtSearch.getText().toString();
+                SearchByName(name);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
-    private void descargardatos(){
+    private void descargardatos() {
         int idpupuseria = Integer.parseInt(id);
         nombre.clear();
         precio.clear();
@@ -66,23 +89,23 @@ private String id, resultado;
         client.get(url, parametros, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-              if(statusCode == 200){
-                  progressDialog.dismiss();
-                  try {
+                if (statusCode == 200) {
+                    progressDialog.dismiss();
+                    try {
 
-                      JSONArray jsonArray = new JSONArray(new String(responseBody));
-                      for (int i = 0; i < jsonArray.length(); i++){
-                          nombre.add(jsonArray.getJSONObject(i).getString("Nombre"));
-                          precio.add(jsonArray.getJSONObject(i).getString("Precio"));
+                        JSONArray jsonArray = new JSONArray(new String(responseBody));
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            nombre.add(jsonArray.getJSONObject(i).getString("Nombre"));
+                            precio.add(jsonArray.getJSONObject(i).getString("Precio"));
 
-                      }
+                        }
 
-                      lista.setAdapter(new CustonAdater(getApplicationContext()));
+                        lista.setAdapter(new CustonAdater(getApplicationContext()));
 
-                  }catch (JSONException e){
-                      e.printStackTrace();
-                  }
-              }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
@@ -92,34 +115,77 @@ private String id, resultado;
         });
     }
 
-    private class CustonAdater extends BaseAdapter{
-            Context ctx;
+    private void SearchByName(String name) {
+        int idPupuseria = Integer.parseInt(id);
+        nombre.clear();
+        precio.clear();
+
+        //final ProgressDialog progressDialog = new ProgressDialog(MenuProductosActivity.this);
+        //progressDialog.setMessage("Cargar Datos...");
+        //progressDialog.show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://pupusasapp.000webhostapp.com/ProductsByName.php";
+        RequestParams parametros = new RequestParams();
+        parametros.put("idpupuseria", idPupuseria);
+        parametros.put("name", name);
+
+        client.get(url, parametros, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200) {
+                    //progressDialog.dismiss();
+                    try {
+
+                        JSONArray jsonArray = new JSONArray(new String(responseBody));
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            nombre.add(jsonArray.getJSONObject(i).getString("Nombre"));
+                            precio.add(jsonArray.getJSONObject(i).getString("Precio"));
+
+                        }
+
+                        lista.setAdapter(new CustonAdater(getApplicationContext()));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+    private class CustonAdater extends BaseAdapter {
+        Context ctx;
         LayoutInflater layoutInflater;
         TextView etnombre, etprecio;
 
-        public CustonAdater(Context applicationContext){
+        public CustonAdater(Context applicationContext) {
             this.ctx = applicationContext;
             layoutInflater = (LayoutInflater) this.ctx.getSystemService(LAYOUT_INFLATER_SERVICE);
 
         }
 
-     @Override
-        public int getCount(){
+        @Override
+        public int getCount() {
             return nombre.size();
-     }
+        }
 
-     @Override
-        public Object getItem(int position){
+        @Override
+        public Object getItem(int position) {
             return position;
-     }
+        }
 
-     @Override
-        public long getItemId(int position){
+        @Override
+        public long getItemId(int position) {
             return position;
-     }
+        }
 
-     @Override
-        public View getView(int position, View converView, ViewGroup parent){
+        @Override
+        public View getView(int position, View converView, ViewGroup parent) {
             ViewGroup viewGroup = (ViewGroup) layoutInflater.inflate(R.layout.lista_producto, null);
             etnombre = (TextView) viewGroup.findViewById(R.id.etProducto);
             etprecio = (TextView) viewGroup.findViewById(R.id.etPrecio1);
@@ -128,7 +194,7 @@ private String id, resultado;
             etprecio.setText(precio.get(position).toString());
 
             return viewGroup;
-     }
+        }
 
     }
 
